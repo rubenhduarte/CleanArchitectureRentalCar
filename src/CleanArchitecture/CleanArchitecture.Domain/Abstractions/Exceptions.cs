3edfb31c40acb35715a2dd94db47d2f348e16112
ruben.duarte <rduarte@ext.laligatech.com>
2024-03-05 19:31:56 -0300

    Agregando exceptiones en abstractions

---
+++ src/CleanArchitecture/CleanArchitecture.Domain/Abstractions/Exceptions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Domain.Abstractions;
+
+public abstract class DomainExceptions : Exception
+{
+}
---
+++ src/CleanArchitecture/CleanArchitecture.Domain/Users/User.cs
@@ -0,0 +1,15 @@
+﻿using CleanArchitecture.Domain.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Domain.Users;
+
+public sealed class User : Entity
+{
+    public User(Guid id) : base(id)
+    {
+    }
+}
